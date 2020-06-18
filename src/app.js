4fd9d7a0c6131e8347d371bf62909d15d47fdeda
James <james.w.schott@gmail.com>
2020-06-17 17:25:16 -0700

    switch to react-amplify

--- src/app.js
+++ src/app.js
@@ -1,49 +1,26 @@
-import API, { graphqlOperation } from '@aws-amplify/api'
-import PubSub from '@aws-amplify/pubsub';
-import { createTodo } from './graphql/mutations'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import awsconfig from './aws-exports';
-API.configure(awsconfig);
-PubSub.configure(awsconfig);
-
-// other imports
-import { onCreateTodo } from './graphql/subscriptions'
-import { listTodos } from './graphql/queries'
-
-const QueryResult = document.getElementById('QueryResult');
-const SubscriptionResult = document.getElementById('SubscriptionResult');
-const MutationButton = document.getElementById('MutationEventButton');
-const MutationResult = document.getElementById('MutationResult');
-
-async function getData() {
-  QueryResult.innerHTML = `QUERY RESULTS`;
-  API.graphql(graphqlOperation(listTodos)).then((evt) => {
-    evt.data.listTodos.items.map((todo, i) => 
-    QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`
-    );
-  })
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-API.graphql(graphqlOperation(onCreateTodo)).subscribe({
-  next: (evt) =>{
-    SubscriptionResult.innerHTML = `SUBSCRIPTION RESULTS`
-    const todo = evt.value.data.onCreateTodo;
-    SubscriptionResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`
-  }
-});
-
-MutationButton.addEventListener('click', (evt) => {
-  MutationResult.innerHTML = `MUTATION RESULTS:`;
-  createNewTodo().then( (evt) => {
-    MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`
-  })
-});
-
-async function createNewTodo() {
-    const todo = { name: "Diglett" , description: "Dig"}
-    return await API.graphql(graphqlOperation(createTodo, { input: todo }))
-  }
-
-getData();
-
+export default App;
